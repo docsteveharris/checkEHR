a6449214457cf200b58c919ec7177f9507cdf964
Steve Harris <doc@steveharris.me>
2018-06-06 22:35:24 +0100

    using unittest in the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,25 +1,39 @@
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-
-# A colleague shares a link I can use to access the database.
-browser.get('http://localhost:5000')
+import unittest
 
 
-# I navigate to that link in my browser, and land on a page which identifies
-# itself as 'checkEHR', and as the page where I can enter a new validation
-# check.
-assert 'checkEHR' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-# I type a correctly a formatted JSON string that contains a list of concept
-# codes, and key:value pairs for context, valid-date, validation check name,
-# and notes.
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# I type enter and the data is saved, and I am re-directed to a page with a
-# list of checks showing a list of  'validation check names', and links back
-# to the data entry page.
+    def tearDown(self):
+        self.browser.quit()
 
-# I repeat the process, and see the list extend by one each time.
+    def test_can_enter_json_and_retrieve_it_later(self):
+        # A colleague shares a link I can use to access the database.
+        self.browser.get('http://localhost:5000')
 
-# I click on one of those links and see the JSON displayed.
+        # We check the page title is correctly labelled
+        self.assertIn('checkEHR', self.browser.title)
+        self.fail('do more work, this always fails')
 
+        # I navigate to that link in my browser, and land on a page which
+        # identifies itself as 'checkEHR', and as the page where I can enter a
+        # new validation check.
+
+        # I type a correctly a formatted JSON string that contains a list of
+        # concept codes, and key:value pairs for context, valid-date,
+        # validation check name, and notes.
+
+        # I type enter and the data is saved, and I am re-directed to a page
+        # with a list of checks showing a list of  'validation check names',
+        # and links back to the data entry page.
+
+        # I repeat the process, and see the list extend by one each time.
+
+        # I click on one of those links and see the JSON displayed.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
