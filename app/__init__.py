07878106c2763eec06ebbc336d668aa41ed97b04
Steve Harris <doc@steveharris.me>
2018-06-09 07:16:14 +0100

    simplified changes needed to implement first test

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 
@@ -6,23 +5,10 @@ def create_app(test_config=None):
     '''
     create and configure your application
     '''
-    app = Flask(__name__, instance_relative_config=True)
-
-    if test_config is None:
-        # load the instance config when not testing
-        pass
-    else:
-        # load the test config
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app = Flask(__name__)
 
     # simple home page that says hello
-    @app.route('/', methods=['GET'])
+    @app.route('/')
     def index():
         return 'Hello World!<br/>You are home'
 
