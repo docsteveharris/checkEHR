3b408c3ae90126192d4fbb49e201a079e6368328
Steve Harris <doc@steveharris.me>
2018-07-10 06:53:31 +0100

    g context used to share docs with index url

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,16 @@
 from . import main
-from flask import render_template
+from flask import render_template, g
 from .. import db
+# from flask_cloudant import FlaskCloudant
 
 
 @main.route('/')
 def index():
-    return render_template('index.html', all_docs=db.all_docs()['rows'])
+    all_docs = db.all_docs(include_docs=True)
+    # attach docs to g context so available without passing as separate
+    # argument
+    g.docs = all_docs['rows']
+    return render_template('index.html')
 
 
 @main.route('/testthis')
