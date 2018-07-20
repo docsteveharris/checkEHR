9945af8f30b3c7cc4256d521b6e4f16ae6e5a8aa
Steve Harris <doc@steveharris.me>
2018-07-20 23:12:19 +0100

    functional tests now grouped

--- tests/func/test_walkthrough.py
+++ tests/func/test_walkthrough.py
@@ -1,16 +1,28 @@
 # Functional tests using pytest-selenium
+# - [ ] @NOTE: (2018-07-20) following https://github.com/dihnatsyeu
+#   /blazemeter-TestNG/blob/master/tests/booking_test.py
+import pytest
+# from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-def test_homepage(selenium):
-    selenium.get("http://127.0.0.1:5000")
-    assert selenium.title == 'checkEHR'
+@pytest.mark.usefixtures('driver_init')
+class BaseTest:
+    pass
 
 
-# - [ ] @TODO: (2018-07-19) @resume stop calling browser twice
-def test_database_connected(selenium):
-    selenium.get("http://127.0.0.1:5000")
-    body = selenium.find_element_by_id('database_url')
-    time.sleep(2)
-    assert 'Database' in body.text
-    assert 'testing' in body.text
+class TestHomePage(BaseTest):
+
+    def test_homepage(self):
+        wait = WebDriverWait(self.driver, 3)
+        self.driver.get("http://127.0.0.1:5000")
+        assert self.driver.title == 'checkEHR'
+
+    def test_database_connected(self):
+        self.driver.get("http://127.0.0.1:5000")
+        body = self.driver.find_element_by_id('database_url')
+        time.sleep(2)
+        assert 'Database' in body.text
+        assert 'testing' in body.text
